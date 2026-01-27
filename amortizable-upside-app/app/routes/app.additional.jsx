8340608db147217501f5721e0a80253071c08d2c
Tony Billing <emely44daretfb@hotmail.com>
2026-01-27 00:47:00 +0000

    update app router

--- amortizable-upside-app/app/routes/app.additional.jsx
+++ amortizable-upside-app/app/routes/app.additional.jsx
@@ -21,16 +21,7 @@ export default function AdditionalPage() {
         </s-paragraph>
       </s-section>
       <s-section slot="aside" heading="Resources">
-        <s-unordered-list>
-          <s-list-item>
-            <s-link
-              href="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-              target="_blank"
-            >
-              App nav best practices
-            </s-link>
-          </s-list-item>
-        </s-unordered-list>
+
       </s-section>
     </s-page>
   );
